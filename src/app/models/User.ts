572150405b8b553a5f9e407c6c2dadfb8a5a55ac
Leonardo Di Salvo <leonardodisalvo99@gmail.com>
2022-03-18 14:24:57 +0100

    solved exercise 2

---
+++ src/app/models/User.ts
@@ -0,0 +1,29 @@
+export type Role = 'staff' | 'student' | 'manager' | 'admin';
+
+export type Gender = 'male' | 'female' | 'other';
+
+export interface Address {
+  city: string;
+  street: string;
+  postalCode: string;
+}
+export interface Companies {
+  id: number;
+  name: string;
+  description: string;
+  location: Address;
+}
+
+export interface Person {
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
+  dateOfBirth: string;
+  address: Address;
+  role: Role;
+  username: string;
+  profilPhotoUrl: string;
+  companies: Companies[];
+  gender: Gender;
+}
